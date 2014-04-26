6fdc8dc943ddc5c2eea72256e6e66e910ed8307b
Joasia <joasia@MacBook-Air-Joasia.local>
2014-04-26 13:07:57 +0200

    RWD

--- js/main.js
+++ js/main.js
@@ -1,19 +1,6 @@
-
-
-$(document).ready(function(){
-
-// var scrollPos=scrollTop();
-var wind_scr = $(window).scrollTop();
-if(wind_scr >= 195){
-    $('.container.banner').addClass('hidden');
-    $('.navbar').addClass('move-top');
-    // $('.main-container').addClass('margin-top-100');
-}
-
-else{
-    $('.container.banner').removeClass('hidden');
-    $('.container.header').removeClass('move-top');
+$('#countdown').countdown({
+    schedule: { 
+        "Monday": [ "19:00:00"],
+        "Friday": ["14:02:00"]
     }
-
-
-    });
+});
